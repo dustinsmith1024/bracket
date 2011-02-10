965579160d0dbc15f4c8ec2f4cac59718e3c2ab2
Dustin Smith <dustinsmith1024@hotmail.com>
2011-02-09 21:33:40 -0600

    change jquery UI to custom - customize next

--- public/javascripts/pages.js
+++ public/javascripts/pages.js
@@ -10,10 +10,17 @@ $("a.ajax").live("click",function(event){
   $.ajax({
     url: url + "?format=js", type: 'get', dataType: 'html',
     success: function(data, status, xhr){
-      $("#popup").html(data).dialog({ width: 750, height: 550, maxHeight: 600, title: title });
+//      $("#popup").html(data).dialog({ width: 750, height: 550, maxHeight: 600, title: title });
+      $("#popup > p.body").html(data).parent().show();
     }
   });
 });
 
+$(".popup").toggle();
+
+$(".popup-close").live("click",function(event){
+  event.preventDefault();
+  $(this).parent().parent().hide();
+});
 
 });
